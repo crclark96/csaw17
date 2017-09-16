03ed5dbc961cc3b5fe5b82764460252c8d23696d
Collin Clark <crclark96@gmail.com>
2017-09-16 10:46:17 -0400

    record valid bytestreams

--- misc/serial/validate.py
+++ misc/serial/validate.py
@@ -1,18 +1,24 @@
 import sys, socket
 
+f = open('bytestream','w')
+
 def validate(data):
-#    print repr(data)
-    data = str(data)[2:len(data)-2][::-1] # trim and reverse data
+    print repr(data), " | ",
+    data = str(data)[1:len(data)-2]# trim data
     tot = 0
     for char in data:
         tot += int(char)
-    if tot%2 == 0:
+    data = data[1:]
+    print repr(data), " | ",
+    if tot % 2 == 0:
         sum = 0
         for i in range(8):
-            sum += 2**(7-i) * int(data[i])
-        print data
-        print chr(sum)
-    return tot%2==0
+            sum += 2**i * int(data[i])
+        print(chr(sum))
+        f.write(chr(sum))
+    else:
+        print 
+    return tot % 2 == 0
 
 def netcat(host, port):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -36,4 +42,4 @@ def netcat(host, port):
     
 
 netcat('misc.chal.csaw.io', 4239)
-
+print '\n'
