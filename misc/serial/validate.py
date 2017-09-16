ad4fc9aa93602bec814f25b86804b1ce684f955e
Collin Clark <crclark96@gmail.com>
2017-09-16 00:46:19 -0400

    add network capability

--- misc/serial/validate.py
+++ misc/serial/validate.py
@@ -1,40 +1,39 @@
 import sys, socket
 
-def netcat(host, port, content):
+def validate(data):
+#    print repr(data)
+    data = str(data)[2:len(data)-2][::-1] # trim and reverse data
+    tot = 0
+    for char in data:
+        tot += int(char)
+    if tot%2 == 0:
+        sum = 0
+        for i in range(8):
+            sum += 2**(7-i) * int(data[i])
+        print data
+        print chr(sum)
+    return tot%2==0
+
+def netcat(host, port):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect((host, int(port)))
-    s.sendall(content.encode())
-    i = 0
+    data = s.recv(256).split('\n')[1]
+    if validate(data+'\n'):
+        s.send("1")
+    else:
+        s.send("0")
     while True:
-        i += 1
-        data = s.recv(4096).split('\n')
-        try:
-            data = data[1]
-        except:
-            data = data[0]
-        if not data: 
+        data = s.recv(256)
+        if not data:
             break
-        tot = 0
-        for char in data[1:len(data)-1]: # validate parity
-            tot += int(char) # sum data byte
-        if tot%2 == 0:
-            print data[1:len(data)-3] # print data w/o parity, stop, start
-            s.sendall("1".encode())
+        if validate(data):
+            s.send("1")
         else:
-            print "invalid"
-            s.sendall("0".encode())
-        print data
-
+            s.send("0")
+    
     s.shutdown(socket.SHUT_WR)
     s.close()
     
 
-netcat('misc.chal.csaw.io', 4239, '')
-
-i = 0
-
-for line in sys.stdin:
-    i+=1
-    if i==1:
-        continue
+netcat('misc.chal.csaw.io', 4239)
 
